78baf42ee7794b589499e3097b3baaaa5c75504f
Kitija Ieva Bērziņa <s20berzkiti@venta.lv>
2023-03-11 17:24:27 +0200

    Edit: added variables and constructors

--- src/DataStr/MyArrayList.java
+++ src/DataStr/MyArrayList.java
@@ -1,5 +1,22 @@
 package DataStr;
 
 public class MyArrayList {
+	private char[] elements;
+	private final int DEFAULT_ARRAY_SIZE = 6; //konstante
+	private int arraySize = DEFAULT_ARRAY_SIZE;
+	private int elementCounter = 0;
+	
+	//bez argumentu konstruktors
+	public MyArrayList() {
+		elements = new char[arraySize]; //masivs ar 6 šūnām
+	}
+	//args constructor
+	public MyArrayList(int inputArraySize) {
+		if(inputArraySize > 0) {
+			arraySize = inputArraySize;
+		}
+		
+		elements = new char[arraySize];
+	}
 
 }
