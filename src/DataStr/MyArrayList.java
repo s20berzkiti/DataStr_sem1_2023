b768fae0705d0f1bdf578335685d28efd7f4393e
Kitija Ieva Bērziņa <s20berzkiti@venta.lv>
2023-03-11 17:46:54 +0200

    Edit: added isfull isempty funkcijas

--- src/DataStr/MyArrayList.java
+++ src/DataStr/MyArrayList.java
@@ -18,5 +18,27 @@ public class MyArrayList {
 		
 		elements = new char[arraySize];
 	}
+	
+	//isEmpty function
+	public boolean isEmpty() {
+		/*long if-else
+		if(elementCounter == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		*/
+		//short if-else
+		//kopejais (if izteiksme)       ? ko darit true gadijuma : ko darit false gadijuma;
+		//return     (elementCounter == 0)? true                   : false;
+		return     (elementCounter == 0);
+	}
+	
+	//isFull function
+	public boolean isFull() {
+		return (elementCounter == arraySize);
+			
+	}
 
 }
